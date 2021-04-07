992273aeac9b95171aa5380ce0f99a636a893950
zhengcaifu <344984602@qq.com>
2021-04-07 16:05:31 +0400

    最长回字串。

--- lib/src/main/java/com/test/lib/dynamic_programming/LongestPalindrome.java
+++ lib/src/main/java/com/test/lib/dynamic_programming/LongestPalindrome.java
@@ -52,7 +52,28 @@ public class LongestPalindrome {
         return result;
     }
 
+    public static String longestPalindrome2(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        String ans = "";
+        for (int l = 0; l < n; ++l) {
+            for (int i = 0; i + l < n; ++i) {
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = true;
+                } else if (l == 1) {
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                } else {
+                    dp[i][j] = s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1];
+                }
+                if (dp[i][j] && l + 1 > ans.length()) {
+                    ans = s.substring(i, i + l + 1);
+                }
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
-        System.out.println("result:" + longestPalindrome("babad"));
+        System.out.println("result:" + longestPalindrome2("babad"));
     }
 }
