ffb76f457bee33aa1b5fb40bf1e90b3e65935667
zhengcaifu <344984602@qq.com>
2021-06-23 09:43:43 +0400

    剑指 Offer 15. 二进制中1的个数 请实现一个函数，输入一个整数（以二进制串形式），输出该数二进制表示中 1 的个数。例如，把 9 表示成二进制是 1001，有 2 位是 1。因此，如果输入 9，则该函数输出 2。
    示例 1：
    
    输入：00000000000000000000000000001011
    输出：3
    解释：输入的二进制串 00000000000000000000000000001011 中，共有三位为 '1'。

---
+++ lib/src/main/java/com/test/lib/daily/HammingWeight.java
@@ -0,0 +1,32 @@
+package com.test.lib.daily;
+
+/**
+ * 剑指 Offer 15. 二进制中1的个数
+ * 请实现一个函数，输入一个整数（以二进制串形式），输出该数二进制表示中 1 的个数。例如，把 9 表示成二进制是 1001，有 2 位是 1。因此，如果输入 9，则该函数输出 2。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：00000000000000000000000000001011
+ * 输出：3
+ * 解释：输入的二进制串 00000000000000000000000000001011 中，共有三位为 '1'。
+ */
+
+public class HammingWeight {
+
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n >> i & 1) == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("result:" + new HammingWeight().hammingWeight(00000000000000000000000000001011));
+    }
+
+}
